0092042ce3ab4822f4f9b0fc265601cb3c4ff154
fernando <fernandomede@gmail.com>
2025-04-22 00:50:31 -0300

    refatoração geral

--- cypress/e2e/cadastro/registra-conta.cy.js
+++ cypress/e2e/cadastro/registra-conta.cy.js
@@ -8,9 +8,9 @@ describe('Registro de conta no website LambdaTest', () => {
 
   beforeEach(() => {
     cy.visit('/');
-    cy.get('#main-navigation').contains(' My account ').trigger('mouseover').as('submenu')
-    cy.get("@submenu").get('li').contains(' Register ').click();
-    cy.url().should('eq', `${Cypress.config().baseUrl}/index.php?route=account/register`)
+    cy.get('#main-navigation').contains(' My account ').trigger('mouseover').as('submenu');
+    cy.get('@submenu').get('li').contains(' Register ').click();
+    cy.url().should('eq', `${Cypress.config().baseUrl}/index.php?route=account/register`);
   });
 
   it('ct01 - exibe mensagem de erro ao tentar registrar com campos obrigatórios em branco', () => {
@@ -22,87 +22,128 @@ describe('Registro de conta no website LambdaTest', () => {
     cy.get('#input-confirm').should('be.visible').and('be.enabled').clear();
     cy.get('input[type=submit]').should('be.visible').and('be.enabled').click();
 
-    const items = []
-    cy.get('.text-danger', { timeout: 10000 }).each(($div) => items.push($div.text()))
-    cy.wrap(items).should('deep.equal', [
+    const expectErros = [
       'First Name must be between 1 and 32 characters!',
       'Last Name must be between 1 and 32 characters!',
       'E-Mail Address does not appear to be valid!',
       'Telephone must be between 3 and 32 characters!',
-      'Password must be between 4 and 20 characters!'
-    ])
+      'Password must be between 4 and 20 characters!',
+    ];
+
+    cy.get('.text-danger', { timeout: 10000 }).each(($div, index) => {
+      cy.wrap($div).should('contain.text', expectErros[index]);
+    });
   });
 
   it('ct05 - exibe mensagem de erro ao inserir senhas diferentes nos campos de senha e confirmação', () => {
-    cy.get('#input-firstname').should('be.visible').and('be.enabled').type(faker.person.firstName());
-    cy.get('#input-lastname').should('be.visible').and('be.enabled').type(faker.person.lastName());
-    cy.get('#input-email').should('be.visible').and('be.enabled').type(faker.internet.email());
-    cy.get('#input-telephone').should('be.visible').and('be.enabled').type(faker.phone.number({ style: 'national' }))
-    cy.get('#input-password').should('be.visible').and('be.enabled').type(password)
-    cy.get('#input-confirm').should('be.visible').and('be.enabled').type("123qwe@@");
-    cy.get('#input-agree').should('be.enabled').check({ force: true })
-    cy.get('input[type=submit]').should('be.visible').and('be.enabled').click();
+    cy.preencherFormRegistro({
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      telephone: faker.phone.number({ style: 'national' }),
+      password: password,
+      confirmPassword: '123qwe@@',
+      agreeTerms: true,
+    });
 
-    cy.get('.text-danger').should('be.visible').and('contain', 'Password confirmation does not match password!')
+    const expectErros = [
+      'Password confirmation does not match password!',
+    ];
+
+    cy.get('.text-danger', { timeout: 10000 }).each(($div, index) => {
+      cy.wrap($div).should('contain.text', expectErros[index]);
+    });
   });
 
   it('ct06 - impede o registro quando os termos e condições não são aceitos', () => {
-    cy.get('#input-firstname').should('be.visible').and('be.enabled').type(faker.person.firstName());
-    cy.get('#input-lastname').should('be.visible').and('be.enabled').type(faker.person.lastName());
-    cy.get('#input-email').should('be.visible').and('be.enabled').type(faker.internet.email());
-    cy.get('#input-telephone').should('be.visible').and('be.enabled').type(faker.phone.number({ style: 'national' }))
-    cy.get('#input-password').should('be.visible').and('be.enabled').type(password)
-    cy.get('#input-confirm').should('be.visible').and('be.enabled').type(password)
-    cy.get('input[type=submit]').should('be.visible').and('be.enabled').click();
+    cy.preencherFormRegistro({
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      telephone: faker.phone.number({ style: 'national' }),
+      password: password,
+      confirmPassword: password,
+      agreeTerms: false,
+    });
 
-    cy.contains('#account-register > .alert', ' Warning: You must agree to the Privacy Policy!').should('be.visible')
+    const expectErros = [
+      ' Warning: You must agree to the Privacy Policy!',
+    ];
+
+    cy.get('#account-register > .alert', { timeout: 10000 }).each(($div, index) => {
+      cy.wrap($div).should('contain.text', expectErros[index]);
+    });
   });
 
   it('ct07 - registra o usuário ao preencher todos os campos obrigatórios e aceitar os termos', () => {
-    cy.get('#input-firstname').should('be.visible').and('be.enabled').type(faker.person.firstName());
-    cy.get('#input-lastname').should('be.visible').and('be.enabled').type(faker.person.lastName());
-    cy.get('#input-email').should('be.visible').and('be.enabled').type(faker.internet.email());
-    cy.get('#input-telephone').should('be.visible').and('be.enabled').type(faker.phone.number({ style: 'national' }))
-    cy.get('#input-password').should('be.visible').and('be.enabled').type(password);
-    cy.get('#input-confirm').should('be.visible').and('be.enabled').type(password);
-    cy.get('#input-agree').should('be.enabled').check({ force: true })
-    cy.get('input[type=submit]').should('be.visible').and('be.enabled').click();
+    cy.preencherFormRegistro({
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      telephone: faker.phone.number({ style: 'national' }),
+      password: password,
+      confirmPassword: password,
+      agreeTerms: true,
+    },
+      `${Cypress.config().baseUrl}/index.php?route=account/success`
+    );
 
-    cy.url().should('eq', `${Cypress.config().baseUrl}/index.php?route=account/success`)
-    cy.contains('h1', ' Your Account Has Been Created!').should('be.visible')
+    const expectErros = [
+      ' Your Account Has Been Created',
+    ];
+
+    cy.get('h1', { timeout: 10000 }).each(($div, index) => {
+      cy.wrap($div).should('contain.text', expectErros[index]);
+    });
   });
 
   it('ct09 - bloqueia o registro ao usar um e-mail já associado a uma conta existente', () => {
-    cy.get('#input-firstname').should('be.visible').and('be.enabled').type(faker.person.firstName());
-    cy.get('#input-lastname').should('be.visible').and('be.enabled').type(faker.person.lastName());
-    cy.get('#input-email').should('be.visible').and('be.enabled').type(email);
-    cy.get('#input-telephone').should('be.visible').and('be.enabled').type(faker.phone.number({ style: 'national' }))
-    cy.get('#input-password').should('be.visible').and('be.enabled').type(password)
-    cy.get('#input-confirm').should('be.visible').and('be.enabled').type(password)
-    cy.get('#input-agree').should('be.enabled').check({ force: true })
-    cy.get('input[type=submit]').should('be.visible').and('be.enabled').click();
+    cy.preencherFormRegistro({
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: email,
+      telephone: faker.phone.number({ style: 'national' }),
+      password: password,
+      confirmPassword: password,
+      agreeTerms: true,
+    },
+      `${Cypress.config().baseUrl}/index.php?route=account/register`
+    );
 
-    cy.url().should('eq', `${Cypress.config().baseUrl}/index.php?route=account/register`)
-    cy.contains('#account-register > .alert', ' Warning: E-Mail Address is already registered!').should('be.visible')
+    const expectErros = [
+      ' Warning: E-Mail Address is already registered!',
+    ];
+
+    cy.get('#account-register > .alert', { timeout: 10000 }).each(($div, index) => {
+      cy.wrap($div).should('contain.text', expectErros[index]);
+    });
   });
 
   it('ct10 - registra o usuário e inscreve no boletim informativo ao marcar a opção correspondente', () => {
-    cy.get('#input-firstname').should('be.visible').and('be.enabled').type(faker.person.firstName());
-    cy.get('#input-lastname').should('be.visible').and('be.enabled').type(faker.person.lastName());
-    cy.get('#input-email').should('be.visible').and('be.enabled').type(faker.internet.email());
-    cy.get('#input-telephone').should('be.visible').and('be.enabled').type(faker.phone.number({ style: 'national' }))
-    cy.get('#input-password').should('be.visible').and('be.enabled').type(password);
-    cy.get('#input-confirm').should('be.visible').and('be.enabled').type(password);
-    cy.get('.custom-control-label').first().click();
-    cy.get('#input-agree').should('be.enabled').check({ force: true })
-    cy.get('input[type=submit]').should('be.visible').and('be.enabled').click();
+    cy.preencherFormRegistro({
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      telephone: faker.phone.number({ style: 'national' }),
+      password: password,
+      confirmPassword: password,
+      subscribeNewsletter: true,
+      agreeTerms: true,
+    },
+      `${Cypress.config().baseUrl}/index.php?route=account/success`
+    );
 
-    cy.url().should('eq', `${Cypress.config().baseUrl}/index.php?route=account/success`)
-    cy.contains('h1', ' Your Account Has Been Created!').should('be.visible')
+    const expectErros = [
+      ' Your Account Has Been Created!',
+    ];
+
+    cy.get('h1', { timeout: 10000 }).each(($div, index) => {
+      cy.wrap($div).should('contain.text', expectErros[index]);
+    });
   });
- 
+
   it('ct11 - exibe mensagem com a política de privacidade', () => {
     cy.contains('a', 'Privacy Policy').should('be.visible').click();
-    cy.get('.modal-content').should('be.visible').and('contain', 'Privacy Policy')
+    cy.get('.modal-content').should('be.visible').and('contain', 'Privacy Policy');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,22 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('preencherFormRegistro', (userData, expectedUrl = null) => {
+  cy.get('#input-firstname').clear().type(userData.firstName || '');
+  cy.get('#input-lastname').clear().type(userData.lastName || '');
+  cy.get('#input-email').clear().type(userData.email || '');
+  cy.get('#input-telephone').clear().type(userData.telephone || '');
+  cy.get('#input-password').clear().type(userData.password || '');
+  cy.get('#input-confirm').clear().type(userData.confirmPassword || '');
+
+  if (userData.agreeTerms) {
+    cy.get('#input-agree').check({ force: true });
+  }
+
+  if (userData.subscribeNewsletter) {
+    cy.get('.custom-control-label').first().click();
+  }
+
+  cy.get('input[type=submit]').click();
+
+  if (expectedUrl) {
+    cy.url().should('eq', expectedUrl)
+  }
+});
